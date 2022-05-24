92bf51a4ad57d97a2e55499aeb567f8230ad1c70
YayoReynoso <eduardo_3reynoso@hotmail.com>
2022-05-24 11:29:46 -0700

    Codigo del programa

--- Program.cs
+++ Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Ejercicio1
 {
+    //Reynoso Rosales Eduardo Guadalupe #21212036
     class Program
     {
         static void Main(string[] args)
         {
+            int nEntero;
+            try
+            {
+                 Console.Write("Indica un numero entero para calcular su cuadrado: ");
+                 nEntero = int.Parse(Console.ReadLine());
+
+
+                 Console.WriteLine("Numero al cuadrado: " + Math.Pow(nEntero, 2));
+
+            }
+            
+            catch (FormatException)
+            {
+                 Console.WriteLine("Debes ingresar un numero ENTERO. \nNo se admite el uso de decimales y/o letras !!!");
+                 Console.ReadKey();
+            }
+            
+            catch (OverflowException)
+            {
+                 Console.WriteLine("Tu numero es muy grande !!!");
+            }
+            
+
+            
+            
+
+                
+            
+            
         }
     }
 }
